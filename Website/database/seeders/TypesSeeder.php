72277e828079e19d7f5556bf89ea5ad785ca40f2
Pepijn <128467109+Schaapie09@users.noreply.github.com>
2024-03-03 21:39:53 +0100

    update data seeder
    updated the data seeder, to include categories and and_types

---
+++ Website/database/seeders/TypesSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AdType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AdType::create(['name' => 'Kopen']);
+        AdType::create(['name' => 'Huren']);
+    }
+}
--- Website/resources/views/ad/create.blade.php
+++ Website/resources/views/ad/create.blade.php
@@ -6,16 +6,22 @@
 
         <label for="title">Title</label>
         <input id="title" type="text" name="title">
+        <br/>
 
         <label for="description">Description</label>
         <input id="description" type="text" name="description">
+        <br/>
 
         <label for="price">Price</label>
         <input id="price" type="number" name="price">
+        <br/>
 
         <label for="postalcode">Postalcode</label>
         <input id="postalcode" type="text" name="postalcode">
+        <br/>
 
+        <label for="category">Categories</label>
+        <br/>
         @foreach($categories as $category)
             <label>
                 <input id="category" type="radio" name="category" value="{{ $category->id }}">
@@ -23,6 +29,9 @@
             </label>
             <br/>
         @endforeach
+        <br/>
+        <label for="type">Types</label>
+        <br/>
         @foreach($types as $type)
             <label>
                 <input id="type" type="radio" name="type" value="{{ $type->id }}">
