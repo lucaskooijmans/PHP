e483c0750883b5cc25b61f41ecf3a3a61ca76c2c
Pepijn <128467109+Schaapie09@users.noreply.github.com>
2024-03-03 12:47:30 +0100

    'role'
    Added 'role' table to the database.
    Added a seeder for the 'role' table.

--- Website/resources/views/auth/register.blade.php
+++ Website/resources/views/auth/register.blade.php
@@ -39,6 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <div class="mt-4">
+            <x-input-label for="type" :value="__('Type')" />
+
+            <x-dropdown-input id="type" class="block mt-1 w-full"
+                          type="type"
+                          name="type"/>
+
+            <x-input-error :messages="$errors->get('type')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
