247f7b1f31af462ea03d8eeed2410f4f461d275b
lucas <lam.kooijmans@student.avans.nl>
2024-03-11 15:06:34 +0100

    tidy up nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,14 +1,26 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ URL::asset('css/nav.css') }}">
+</head>
+
 <nav class="container-fluid">
     <ul>
         <li><strong>Marketplace</strong></li>
     </ul>
     <ul>
         <li><a href="{{route('ad.index')}}" role="button">Home</a></li>
-        <li><a href="{{route('order.index')}}" role="button">My orders</a></li>
         <li><a href="{{route('ad.all')}}" role="button">All advertisements</a></li>
-        <li><a href="{{route('ad.myFavorites')}}" role="button">My favorites</a></li>
-        <li><a href="{{route('ad.my')}}" role="button">My advertisements</a></li>
-        <li><a href="{{route('ad.create')}}" role="button">Create advertisement</a></li>
+
+        <li class="dropdown">
+            <a href="#" role="button">My profile</a>
+            <ul class="dropdown-content">
+                <li><a href="{{route('ad.create')}}" role="button">Create advertisement</a></li>
+                <li><a href="{{route('ad.my')}}" role="button">My advertisements</a></li>
+                <li><a href="{{route('ad.myFavorites')}}" role="button">My favorites</a></li>
+                <li><a href="{{route('order.index')}}" role="button">My orders</a></li>
+            </ul>
+        </li>
+
         <li><a href="#" role="button">Contact</a></li>
         @if(!Auth::check())
             <li><a href="{{route('login')}}">LOGIN</a></li>
