1e8b2508fefb5304f2d8e8d32b03ebe0f8c63274
Pepijn <128467109+Schaapie09@users.noreply.github.com>
2024-03-27 12:15:15 +0100

    Fixed Sanctum when not logged in
    Title

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -8,7 +8,6 @@
         <li><a href="{{route('ad.all')}}" role="button">ALL</a></li>
         <li><a href="{{route('ad.my')}}" role="button">MY</a></li>
         <li><a href="{{route('ad.create')}}" role="button">CREATE</a></li>
-        <li><a href="{{route('account.index', Auth::id())}}" role="button">Account</a></li>
         @if(!Auth::check())
             <li><a href="{{route('login')}}">LOGIN</a></li>
             <li><a href="{{route('register')}}">REGISTER</a></li>
@@ -19,6 +18,7 @@
                     <button type="submit"> > LOG OUT</button>
                 </form>
             </li>
+            <li><a href="{{route('account.index', Auth::id())}}" role="button">Account</a></li>
         @endif
     </ul>
 </nav>
